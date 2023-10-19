3a6c27f8e66eb9f51991768ad8b9bfc2d4106072
josecastromedi <128188742+josecastromedi@users.noreply.github.com>
2023-10-18 19:26:21 -0600

    Changes User and Login

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -4,13 +4,15 @@
  */
 package controller;
 
-import java.io.FileInputStream;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.StringTokenizer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -18,9 +20,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-/*
-import org.apache.poi.ss.usermodel.*;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;*/
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
 
 /**
  * FXML Controller class
@@ -44,7 +47,54 @@ public class LoginController implements Initializable {
 
     @FXML
     private void onLogin(ActionEvent event) {
+        String username = tf_username.getText();
+        String password = tf_password.getText();
+
+        if (validarCredenciales(username, password)) {
+            mostrarAlerta(AlertType.INFORMATION, "Inicio de sesión exitoso", "¡Bienvenido, " + username + "!");
+            
+        } else {
+            mostrarAlerta(AlertType.ERROR, "Error de inicio de sesión", "Credenciales incorrectas.");
+        }
+        
+        
     }
+    
+    private void mostrarAlerta(AlertType tipo, String titulo, String contenido) {
+        Alert alert = new Alert(tipo);
+        alert.setTitle(titulo);
+        alert.setHeaderText(null);
+        alert.setContentText(contenido);
+        alert.showAndWait();
+    }
+
+   
+   private boolean validarCredenciales(String username, String password) {
+        String rutaArchivo = "C:\\Documentos\\PARADIGMAS DE PROGRAMACION\\Base de datos para proyecto#2\\usuarios.txt";
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(rutaArchivo))) {
+            String linea;
+            while ((linea = reader.readLine()) != null) {
+                StringTokenizer tokenizer = new StringTokenizer(linea, ", ");
+                String nombre = tokenizer.nextToken().trim();
+                String apellidos = tokenizer.nextToken().trim();
+                String usuario = tokenizer.nextToken().trim();
+                String email = tokenizer.nextToken().trim();
+                String contraseña = tokenizer.nextToken().trim();
+
+                if (usuario.equals(username) && contraseña.equals(password)) {
+                    return true; // Credenciales válidas
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return false; // Credenciales no encontradas o inválidas
+    }
+   
+   
+        
 
     @FXML
     private void onRegistrar(ActionEvent event)  {
@@ -53,13 +103,14 @@ public class LoginController implements Initializable {
                 Parent root = loader.load();
 
                 Stage stage = new Stage();
+
                 stage.setTitle("Agregar usuario");
+
+                stage.setTitle("Registro de usuarios");
                 stage.setScene(new Scene(root));
                 stage.show();
-
                 Stage loginStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
                 loginStage.close();
-
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -86,5 +137,6 @@ public class LoginController implements Initializable {
             e.printStackTrace();
         }*/
     }
+    
     
 }
--- src/controller/RegisterController.java
+++ src/controller/RegisterController.java
@@ -4,6 +4,9 @@
  */
 package controller;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -42,6 +45,34 @@ public class RegisterController implements Initializable {
 
     @FXML
     private void onRegistrar(ActionEvent event) {
+        String nombre = tf_nombre.getText();
+        String apellidos = tf_apellidos.getText();
+        String username = tf_username.getText();
+        String email = tf_email.getText();
+        String password = tf_password.getText();
+
+        // Validación de datos (puedes agregar tu lógica de validación aquí)
+
+        if (!nombre.isEmpty() && !apellidos.isEmpty() && !username.isEmpty() && !email.isEmpty() && !password.isEmpty()) {
+            // Luego, guarda los datos en un archivo .txt
+            guardarUsuarioEnArchivo(nombre, apellidos, username, email, password);
+        } else {
+            // Mostrar mensaje de error al usuario si falta información
+        }
+    }
+    
+    private void guardarUsuarioEnArchivo(String nombre, String apellidos, String username, String email, String password) {
+        String rutaArchivo = "C:\\Documentos\\PARADIGMAS DE PROGRAMACION\\Base de datos para proyecto#2\\usuarios.txt";
+        
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(rutaArchivo, true));
+            writer.write(nombre + ", " + apellidos + ", " + username + ", " + email + ", " + password);
+            writer.newLine();
+            writer.close();
+        } catch (IOException e) {
+            // Manejo de excepciones si ocurre algún error al guardar en el archivo
+            e.printStackTrace();
+        }
     }
     
 }
